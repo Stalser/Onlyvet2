945b02e434a922019e01a222623943947ca47c81
Данила <93940085+Stalser@users.noreply.github.com>
2025-11-13 21:41:11 +0500

    Update page.tsx

--- app/doctor/appointments/page.tsx
+++ app/doctor/appointments/page.tsx
@@ -1,4 +1,4 @@
 // app/doctor/appointments/page.tsx
 'use client';
-import WeeklyGrid from '@/components/doctor/WeeklyGrid';
+import WeeklyGrid from "../../../components/doctor/WeeklyGrid";
 export default function AppointmentsPage(){ return <div className="space-y-4"><WeeklyGrid /></div>; }
